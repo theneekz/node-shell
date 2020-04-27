ba289fc12a1ef58926d5b3c0a45318834bad29ce
Thaddeus Metz <temetz90@gmail.com>
2020-04-27 11:31:54 -0500

    quick commit

--- bash.js
+++ bash.js
@@ -3,6 +3,12 @@ process.stdout.write("prompt > ");
 process.stdin.on("data", (data) => {
   const cmd = data.toString().trim();
 
+  if (cmd === "pwd") {
+    console.log(process.cwd());
+    process.stdout.write("\nprompt > ");
+    return;
+  }
+
   process.stdout.write("you wrote: " + cmd);
   process.stdout.write("\nprompt > ");
 });
